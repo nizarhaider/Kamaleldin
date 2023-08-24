be3b84ea8880ad89bc7398a0b22445b0d5ed073d
Nizar <43636043+nizarhaider@users.noreply.github.com>
2023-08-25 00:27:12 +0530

    mobile responsiveness

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Kamaleldin | Personal Portfolio ';
-export const SITE_DESCRIPTION = 'My Personal Portfolio';
+export const SITE_DESCRIPTION = 'Personal Portfolio';
